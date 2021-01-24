9301022341702376239ec79d5f6cff977290f665
jetsaii <jetsai@hmc.edu>
2021-01-24 11:34:58 -0800

    Enable CORS

--- src/main/java/leetcode/api/controller/UserController.java
+++ src/main/java/leetcode/api/controller/UserController.java
@@ -5,11 +5,13 @@ import leetcode.api.service.StatsService;
 import leetcode.api.service.StatsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private StatsService statsService;
